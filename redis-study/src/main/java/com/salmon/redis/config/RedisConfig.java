7b0c1f7612c8967be04144a9949b0208220c98bb
Salmon <1179732961@qq.com>
2024-05-18 21:14:59 +0800

    修复配置问题

--- redis-study/src/main/java/com/salmon/redis/config/RedisConfig.java
+++ redis-study/src/main/java/com/salmon/redis/config/RedisConfig.java
@@ -25,30 +25,6 @@ import java.net.UnknownHostException;
 @Configuration
 public class RedisConfig {
 
-//    @Bean
-//    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
-//        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-//        redisTemplate.setConnectionFactory(factory);
-//        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
-//        ObjectMapper mapper = new ObjectMapper();
-//        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-//        // mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-//        mapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance,
-//                ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
-//        jackson2JsonRedisSerializer.setObjectMapper(mapper);
-//        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
-//        // key采用String的序列化方式
-//        redisTemplate.setKeySerializer(stringRedisSerializer);
-//        // hash的key也采用String的序列化方式
-//        redisTemplate.setHashKeySerializer(stringRedisSerializer);
-//        // value序列化方式采用jackson
-//        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
-//        // hash的value序列化方式采用jackson
-//        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
-//        redisTemplate.afterPropertiesSet();
-//        return redisTemplate;
-//    }
-
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) throws UnknownHostException {
         // 创建模板
